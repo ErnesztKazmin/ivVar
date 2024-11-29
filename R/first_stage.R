cef314bedb211710e50c66eaa96cd777ebd6f256
Erneszt Kazmin <ernest.kazmin@gmail.com>
2024-11-29 01:13:44 +0100

    Rebuild package

--- R/first_stage.R
+++ R/first_stage.R
@@ -3,17 +3,16 @@
 #' Performs the first stage of two-stage least squares regression in the VAR context.
 #'
 #' @param x A VAR model object (class "varest").
-#' @param instrument A character string for the instrument variable.
+#' @param instrument A vector representing the instrument variable values.
 #' @param instrumented A character string for the instrumented variable.
 #' @return A numeric vector of fitted values from the first-stage regression.
 #' @export
 first_stage <- function (x, instrument, instrumented){
   variables <- colnames(x$datamat)[1:x$K]
   p <- x$p
-  instrument <- data[[instrument]]
-  res <- c(rep(NA, times = p+1), residuals(x)[,instrumented])
-  res_model <- lm(as.numeric(res) ~ as.numeric(instrument))
-  res_model_hat <- c(rep(NA, times = p + 1), res_model$fitted.values)
+  res <- c(rep(NA, times = p), residuals(x)[,instrumented])
+  res_model <- lm(res ~ instrument)
+  res_model_hat <- c(rep(NA, times = p ), res_model$fitted.values)
   return(res_model_hat)
 }
 
